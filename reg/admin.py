70e2faf3cc462121e66bd994366678a940ee2f17
Vikas Kumar Kumawat <vikasku.kal@gmail.com>
2025-01-31 14:44:01 +0530

    Update admin.py

--- reg/admin.py
+++ reg/admin.py
@@ -13,20 +13,6 @@ class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'email', 'subject', 'created_at')
     search_fields = ('full_name', 'email', 'subject')
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_seller', 'last_login_time', 'time_since_last_login')
-    readonly_fields = ('time_since_last_login',)  # Make it visible in the detail page
 
-    def time_since_last_login(self, obj):
-        return obj.time_since_last_login()  # Calls method from model
-
-    time_since_last_login.short_description = "Time Since Last Login"
-
-    # Organizing the fields in the detailed view
-    fieldsets = (
-        ("User Information", {"fields": ("user", "is_seller")}),
-        ("Login Details", {"fields": ("last_login_time", "time_since_last_login")}),
-    )
-
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Profile)
     
